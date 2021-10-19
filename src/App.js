68066a9258c846fbc17b3a104e576505cbeec6ba
SergeyKirintsev <kserg80@gmail.com>
2021-10-19 21:39:42 +0300

    Сверстать карточки

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
+import favorites from './images/favorites.svg';
+import favorites_selected from './images/favorites_selected.svg';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className='header__title'>Event Listing</h1>
+    </header>
+  )
+}
+
+function Filter() {
+  return (
+    <div>Filter</div>
+  )
+}
+
+function Cards({cards}) {
+  const getDate = (date) => date[0] + date[1]
+
+  return (
+    <ul className='cards'>
+      {cards.map(card => (
+        <li className='cards__item'>
+          <img className='cards__img' src={card.image} alt=''/>
+          <p className="cards__date">{getDate(card.date)}</p>
+          <h2 className="cards__title">{card.name}</h2>
+          <img className="cards__favorites" src={favorites} alt="" />
+        </li>
+      ))}
+    </ul>
+  )
+}
+
 function App() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/xsolla/xsolla-frontend-school-2021/main/events.json')
+      .then(res => res.json())
+      .then(res => setCards(res))
+      .catch((err) => console.log(err));
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Header/>
+        <Filter/>
+        <Cards cards={cards}/>
+      </div>
+
     </div>
   );
 }
